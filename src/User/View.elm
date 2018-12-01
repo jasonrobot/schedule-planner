e0b4b05bd1c380393b6248205cd8a82552918cf2
Jason Howell <jasonrobot64@gmail.com>
2018-12-01 11:23:43 -0800

    First mockup

--- src/User/View.elm
+++ src/User/View.elm
@@ -1,4 +1,4 @@
-module User.View exposing (rootView)
+module User.View exposing (rootView, viewAll)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -6,11 +6,30 @@ import Html.Events exposing (..)
 import User.Types exposing (..)
 
 
+
+-- renderEach :  List (Html.Attribute msg) -> List (Html.Html msg) -> Html.Html msg
+
+
+renderEach container containerAttrs itemRenderer list =
+    container containerAttrs (List.map (\item -> text item) list)
+
+
+
+-- List.map (\item -> element attrs [ renderer item ]) list
+
+
 rootView : Model -> Html msg
 rootView model =
     div []
         [ div [] [ text ("User: " ++ model.name) ]
         , div [] [ text "has these tasks:" ]
-        , ul []
-            (List.map (\task -> li [] [ text task.name ]) model.tasks)
+        , ul [ class "user__task-list" ]
+            (List.map
+                (\task -> li [ class "user__task" ] [ text task.name ])
+                model.tasks
+            )
         ]
+
+viewAll : List Model -> Html msg
+viewAll models =
+    div [ class "all-users" ] (List.map (\user -> rootView user) models)
